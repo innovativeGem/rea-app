08c5612e051f760e65191de52ce34a993be60509
Naman Nanavati <naman.nanavati@gmail.com>
2017-09-16 02:05:04 +1000

    add and remove buttons working fine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ViewChildren, QueryList, AfterViewInit, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 import { PropertyService } from './property.service';
 
@@ -12,27 +12,18 @@ import { PropertyComponent } from './property/property.component';
     PropertyService
   ],
 })
-export class AppComponent implements OnInit, AfterViewInit  {
-  self = this;
-  // properties: Observable<any[]>;
-  // savedProperties: Observable<any[]>;
+export class AppComponent implements OnInit  {
   properties: any[] = [];
   savedProperties: any[] = [];
-
-  @ViewChildren('savedprops') props: QueryList<ElementRef>;
-  // @ViewChild(PropertyComponent) prop: PropertyComponent;
 
   constructor(private _propertyService: PropertyService) { }
 
   loadProperties() {
-    // this.properties = this._propertyService.props;
     this._propertyService.getProperties().subscribe((data) => this.properties = data.results);
     this._propertyService.getProperties().subscribe((data) => this.savedProperties = data.saved);
-    // console.log('properties: ' + this.properties);
   }
 
   handleEventSaved(data) {
-    // console.log('received: ' + data.price);
     this.savedProperties.push(data);
     this.properties = this.properties.filter(obj => obj.id !== data.id);
   }
@@ -44,25 +35,7 @@ export class AppComponent implements OnInit, AfterViewInit  {
 
   ngOnInit() {
     this._propertyService.getProperties();
-    // this._propertyService.getProperties2();
     this.loadProperties();
-  }
-
-  ngAfterViewInit() {
-    // this.props.forEach(propInstance => console.log(propInstance));
-    // console.log(this.props);
-    this.props.changes.subscribe(changes => {
-      console.log(changes);
-      // changes.nativeElement.isspbtn = false;
-      /*
-      this.props.forEach(e => {
-        e.nativeElement.isspbtn = true;
-      });
-      */
-    });
-
-    // this.prop.swapBtns(this.prop);
-    // console.log(this.prop);
   }
 
 }
--- src/app/property/property.component.ts
+++ src/app/property/property.component.ts
@@ -8,29 +8,19 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 export class PropertyComponent implements OnInit {
 
   @Input() p;
+  @Input() isspbtn: Boolean = false;
   @Output() saveBtnClicked = new EventEmitter();
   brandColor: String;
   isBtnVisible: Boolean = false;
-  isspbtn: Boolean;
-
-  // @Input() isBtnVisible: Boolean = false;
 
   constructor() { }
 
   handleSaveEvent(obj) {
     this.saveBtnClicked.emit(obj);
-    // this.swapBtns(obj);
-  }
-
-  swapBtns() {
-    // console.log('obj class: ' + obj.getAttribute('class'));
-      this.isspbtn = true;
-      console.log('swapBtns: ');
   }
 
   ngOnInit() {
     this.brandColor = this.p.agency.brandingColors.primary;
-    this.isspbtn = false;
   }
 
 }
